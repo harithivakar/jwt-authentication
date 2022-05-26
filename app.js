d5996837d7a536d77ee31bdae36bdcd6b70e1695
Harithivakar <harithivakar.nadar@gmail.com>
2022-05-26 11:59:49 +0530

    Added login and welcome routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 const router = require('./routes/auth.js');
 
+
 require('./config/database.js').connect();
 
 const express = require('express');
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const User = require('../model/user.js');
+const auth = require('../middleware/auth.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 
 const router = express.Router();
+
+router.post('/welcome', auth, (req, res) => {
+    res.status(200).send('Welcome 🙌 ');
+});
+
 
 
 router.post('/register', async (req, res) => {
@@ -52,7 +58,35 @@ router.post('/register', async (req, res) => {
 });
 
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+
+    try {
+        const { email, password } = req.body;
+
+        if(!email && password) {
+            res.status(400).send("All input is required");
+        }
+
+        const user = await User.findOne({ email });
+
+        if(user && (await bcrypt.compare(password, user.password))) {
+            const token = jwt.sign(
+                { user_id: user._id, email },
+                process.env.TOKEN_KEY,
+                {
+                    expiresIn: '2h',
+                }
+            );
+
+            user.token = token;
+
+            res.status(200).json(user);
+        }
+
+        res.status(400).send('Invalid credentials');
+    }catch(err) {
+        console.log(err);
+    }
 
 });
 
